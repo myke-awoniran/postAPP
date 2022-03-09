8db403ce6a72c37b0d63bcf7810600910f10a70c
Awoniran <awoniranopeyemi@gmail.com>
2022-03-09 10:53:32 +0100

    fix-auth

--- routes/feed.js
+++ routes/feed.js
@@ -3,9 +3,9 @@ const feedController = require('../controllers/feedController');
 const authController = require('../controllers/authController');
 
 router.get('/', feedController.home);
+router.get('/posts', feedController.getPosts);
 router.use(authController.protect);
 //all routes below  are all protected
-router.get('/posts', feedController.getPosts);
 router.post('/post', feedController.addPost);
 router.get('/post/:postId', feedController.getPost);
 router.delete('/post/:postId', feedController.deletePost);
