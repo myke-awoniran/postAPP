6662ed3b8fbdfd906dadd4003e69762339404110
Awoniran <awoniranopeyemi@gmail.com>
2022-03-30 22:18:51 +0100

    cors fixed

--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const authController = require('../controllers/authController');
+
 router.get('/me', authController.protect, authController.getMe);
 router.get('/users', authController.getAllUsers);
 router.post('/signup', authController.signup);
--- routes/feed.js
+++ routes/feed.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+
 const feedController = require('../controllers/feedController');
 const authController = require('../controllers/authController');
 
