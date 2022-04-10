91f75b4481930a83dcfc2fd962e864c1d7da59c7
Awoniran <awoniranopeyemi@gmail.com>
2022-04-10 09:11:20 +0100

    cors

--- app.js
+++ app.js
@@ -11,37 +11,11 @@ const AppError = require('./utils/AppError');
 const errHandler = require('./controllers/errorController');
 
 const app = express();
-// Add headers before the routes are defined
-app.use(function (req, res, next) {
-  // Website you wish to allow to connect
-  // res.setHeader(
-  //   'Access-Control-Allow-Origin',
-  //   'https://task-manager-a.netlify.app'
-  // );
-
-  // Request methods you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Methods',
-    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
-  );
-
-  // Request headers you wish to allow
-  res.setHeader(
-    'Access-Control-Allow-Headers',
-    'X-Requested-With,content-type'
-  );
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  // res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
 const corsOpts = {
-  origin: '*',
+  origin: '',
   methods: ['GET', 'POST', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type'],
+  optionsSuccessStatus: 200,
 };
 
 app.use(cors(corsOpts));
@@ -51,8 +25,8 @@ app.use(bodyParser.json());
 app.use(morgan('combined'));
 
 app.get('/', feedRoutes);
-app.use('/feed', feedRoutes);
-app.use('/auth', authRoutes);
+app.use('/feed', cors(), feedRoutes);
+app.use('/auth', cors(), authRoutes);
 
 app.use('*', (req, res, next) => {
   return next(
