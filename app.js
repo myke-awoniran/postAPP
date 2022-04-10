f43ee5e2abc23208d630922383b5915be30eca09
Awoniran <awoniranopeyemi@gmail.com>
2022-04-10 08:06:36 +0100

    cors fixed

--- app.js
+++ app.js
@@ -11,6 +11,30 @@ const AppError = require('./utils/AppError');
 const errHandler = require('./controllers/errorController');
 
 const app = express();
+// Add headers before the routes are defined
+app.use(function (req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+
+  // Request methods you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, OPTIONS, PUT, PATCH, DELETE'
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With,content-type'
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+
+  // Pass to next layer of middleware
+  next();
+});
 const corsOpts = {
   origin: '*',
   methods: ['GET', 'POST', 'DELETE', 'PATCH'],
